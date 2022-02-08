e262a533ca89a01902cb5eb40c881856e42a3d64
georgelucasgp <djgeorgelucas@hotmail.com>
2022-02-08 00:17:51 -0300

    Alterações de Backend

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php include_once("templates/header.php");
+?>
+<main>
+  <div id="title-container">
+    <h1>Blog Codar</h1>
+    <p>O seu blog de programação</p>
+  </div>
+</main>
+<div class="posts-contai">
+  <?php foreach ($posts as $post) : ?>
+    <div class="post-box">
+      <img src="/img/<?= $post['img'] ?>" alt="<?= $post['title']; ?>">
+      <h2 class="post-title">
+        <a href="/post.php?id=<?= $post['id']; ?>"> <?= $post['title']; ?></a>
+      </h2>
+      <p class="post-description"><?= $post['description']; ?></p>
+      <div class="tags-container">
+      <?php foreach($post['tags'] as $tag): ?>
+        <a href="#"><?= $tag ?></a>
+        <?php endforeach; ?>
+      </div>
+    </div>
+  <?php endforeach; ?>
+</div>
+<?php include_once("templates/footer.php");
+?>
